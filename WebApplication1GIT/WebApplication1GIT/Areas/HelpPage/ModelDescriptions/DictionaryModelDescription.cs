e2f25a652023015cb16452563e0d09d7d3138e6f
BRIAN PIRAGAUTA <brianpiragauta@hotmail.com>
2016-12-18 13:51:03 -0500

    Agregando api

---
+++ WebApplication1GIT/WebApplication1GIT/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplication1GIT.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApplication1GIT/WebApplication1GIT/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace WebApplication1GIT.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
